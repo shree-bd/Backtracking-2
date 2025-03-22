dcf538df3969157971813490539961a4da713abb
SB <57531390+shree-bd@users.noreply.github.com>
2025-03-22 10:15:17 -0400

    Solved Both problems

---
+++ subsets.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        def backtrack(start, subset):
+            result.append(subset[:])
+
+            for i in range(start, len(nums)):
+                subset.append(nums[i])
+                backtrack(i+1, subset)
+                subset.pop()
+
+        backtrack(0,[])
+        return result
+
+# Time Complexity: O(2^N)
+# Space Complexity: O(N)   
